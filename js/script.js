40eaed778a2bfa233ddecd5272aeae85172a32a5
TheQueCode <thequecode@gmail.com>
2025-06-13 11:01:35 -0500

    List books

--- js/script.js
+++ js/script.js
@@ -1,10 +1,55 @@
 // Book Class: represents a book
 
-// UI Class: handle ui tasks
+class Book {
+  constructor (author, title, comment) {
+    this.author = author;
+    this.title = title;
+    this.comment = comment;
+  }
+}
+
+// UI Class: Handle UI tasks
+
+class UI{
+  static displayBooks () {
+    const StoredBooks = [
+      {
+        author: 'John Doe',
+        title: 'Book One',
+        comment: 'Boring'
+      },
+      {
+        author: 'Jane Doe',
+        title: 'Book Two',
+        comment: 'Great book'
+      }
+    ];
+
+    const books = StoredBooks
+
+    books.forEach((book) => UI.addBookToList(book))
+  }
+
+  static addBookToList (book) {
+    const list = document.querySelector('#book-list');
+
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+      <td>${book.author}</td>
+      <td>${book.title}</td>
+      <td>${book.comment}</td>
+      <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+    `;
+
+    list.appendChild(row);
+  }
+}
 
 // Store Class: Handles Storage
 
 // Event: Display a Book
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add a Book
 
